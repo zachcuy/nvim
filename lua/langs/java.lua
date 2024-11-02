ee96021ee79a233f3be74348a10975d0d606d482
zcu <me@zachuy.com>
2024-11-02 12:32:28 -0600

    enabled java for ubuntu desktop only

--- lua/langs/java.lua
+++ lua/langs/java.lua
@@ -1,38 +1,38 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     inlay_hints = { enabled = false },
-  --   },
-  -- },
-  -- {
-  --   "mfussenegger/nvim-dap",
-  --   config = function() end,
-  -- },
-  -- { "nvim-treesitter/nvim-treesitter", opts = {
-  --   ensure_installed = { "java" },
-  -- } },
-  -- {
-  --   "nvim-java/nvim-java",
-  --   config = false,
-  --   dependencies = {
-  --     {
-  --       "neovim/nvim-lspconfig",
-  --       opts = {
-  --         servers = {
-  --           jdtls = {
-  --             -- your jdtls configuration goes here
-  --           },
-  --         },
-  --         setup = {
-  --           jdtls = function()
-  --             require("java").setup({
-  --               -- your nvim-java configuration goes here
-  --             })
-  --           end,
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function() end,
+  },
+  { "nvim-treesitter/nvim-treesitter", opts = {
+    ensure_installed = { "java" },
+  } },
+  {
+    "nvim-java/nvim-java",
+    config = false,
+    dependencies = {
+      {
+        "neovim/nvim-lspconfig",
+        opts = {
+          servers = {
+            jdtls = {
+              -- your jdtls configuration goes here
+            },
+          },
+          setup = {
+            jdtls = function()
+              require("java").setup({
+                -- your nvim-java configuration goes here
+              })
+            end,
+          },
+        },
+      },
+    },
+  },
 }
