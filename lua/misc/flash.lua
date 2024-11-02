818c39b61d9bf1e94cfdba3332ac29674e4ddfcf
zcu <me@zachuy.com>
2024-11-02 11:28:39 -0600

    disabled "S" for flash

--- lua/misc/flash.lua
+++ lua/misc/flash.lua
@@ -10,5 +10,6 @@ return {
   keys = {
     -- { "z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     -- { "Z", mode = { "n", "x", "o" }, function() require("flash").jump({ reverse = true }) end, desc = "Flash" },
+    {"S", false},
   },
 }
