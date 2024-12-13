302f061d7e6659700f5ffe89e4f5b9e684679dba
zcu <me@zachuy.com>
2024-12-13 11:20:37 -0700

    disabled guess_indent

--- lua/misc/guess-indent.lua
+++ lua/misc/guess-indent.lua
@@ -1,7 +1,7 @@
 return {
-  "nmac427/guess-indent.nvim",
-  config = function()
-    local guess_indent = require("guess-indent")
-    guess_indent.setup({})
-  end,
+  -- "nmac427/guess-indent.nvim",
+  -- config = function()
+  --   local guess_indent = require("guess-indent")
+  --   guess_indent.setup({})
+  -- end,
 }
