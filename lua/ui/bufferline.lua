5917f6acc4039fbc083a57eaa81a4693a0b27f2d
zcu <zachary.cromwell.uy@gmail.com>
2025-02-12 11:51:44 -0700

    added bufferline sorting hotkey

--- lua/ui/bufferline.lua
+++ lua/ui/bufferline.lua
@@ -4,8 +4,9 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
+      { "<leader>bs", "<Cmd>BufferLineSortByRelativeDirectory<CR>", desc = "Sort Buffers" },
     },
     opts = {
       options = {
