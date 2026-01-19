06ccfa501b456941a9fb503bd03f6021e3194711
Zach Uy <me@zachuy.com>
2026-01-19 09:33:37 -0700

    added recent to sort by most recently accessed

--- lua/ui/snacks.lua
+++ lua/ui/snacks.lua
@@ -1,4 +1,14 @@
 return {
   "folke/snacks.nvim",
-  opts = { dashboard = { enabled = false } },
+  opts = {
+    dashboard = { enabled = false },
+    picker = {
+      sources = {
+        recent = {
+          -- Force sorting by the last time the file was accessed
+          sort = { fields = { "idx" } },
+        },
+      },
+    },
+  },
 }
