af3a5e865f017edfcf2fe94456bbd3a095808f6f
Zach Uy <me@zachuy.com>
2026-03-28 10:07:41 -0600

    Fixed snacks to work with lazygit and lume theme

--- lua/ui/snacks.lua
+++ lua/ui/snacks.lua
@@ -1,6 +1,15 @@
 return {
   "folke/snacks.nvim",
   opts = {
+    -- Lazygit theme is managed in ~/Library/Application Support/lazygit/config.yml
+    -- (includes os.editPreset = "nvim-remote" and Lume theme colors)
+    lazygit = {
+      configure = false,
+      win = {
+        style = "lazygit",
+        wo = { winhighlight = "NormalFloat:Normal" },
+      },
+    },
     dashboard = { enabled = false },
     picker = {
       sources = {
