f4cab1a49ebc0307ce57cad683bc97031dc13430
Zach Uy <me@zachuy.com>
2026-05-24 19:31:34 -0600

    mod: disable surround padding for opening brackets
    - Override mini.surround to remove space padding for (, [, {

---
+++ lua/misc/mini-surround.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      custom_surroundings = {
+        ["("] = { output = { left = "(", right = ")" } },
+        ["["] = { output = { left = "[", right = "]" } },
+        ["{"] = { output = { left = "{", right = "}" } },
+      },
+    },
+  },
+}
