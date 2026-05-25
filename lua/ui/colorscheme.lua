17280a78932ae69a6dbbfbccc5bb159a72b8c561
Zach Uy <me@zachuy.com>
2026-05-24 19:40:01 -0600

    mod: mint-colored comments
    - Override Comment and @comment highlight groups with dimmed mint

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -6,6 +6,8 @@ return {
     config = function()
       require("lume").setup()
       vim.cmd("colorscheme lume")
+      vim.api.nvim_set_hl(0, "Comment", { fg = "#75d7b3", italic = true })
+      vim.api.nvim_set_hl(0, "@comment", { fg = "#75d7b3", italic = true })
     end,
   },
 }
