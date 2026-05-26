8671be87d690d5257e9a2d874babd2f64eb7ffd0
Zach Uy <me@zachuy.com>
2026-05-26 16:27:00 -0600

    fix: block comment auto-close inserting raw terminal codes
    - Switch from expr keymap to feedkeys for reliable special key handling
    - Pre-encode full key sequence with nvim_replace_termcodes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -20,14 +20,15 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "c", "cpp" },
   callback = function()
-    local left = vim.api.nvim_replace_termcodes("<Left>", true, false, true)
+    local close = vim.api.nvim_replace_termcodes("*  */<Left><Left><Left>", true, false, true)
     vim.keymap.set("i", "*", function()
       local col = vim.fn.col(".")
       local line = vim.fn.getline(".")
       if line:sub(col - 1, col - 1) == "/" then
-        return "*  */" .. left .. left .. left
+        vim.api.nvim_feedkeys(close, "n", false)
+      else
+        vim.api.nvim_feedkeys("*", "n", false)
       end
-      return "*"
-    end, { expr = true, buffer = true })
+    end, { buffer = true })
   end,
 })
